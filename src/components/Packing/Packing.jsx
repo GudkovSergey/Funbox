62d3947db12a6af0f8511411a79cfcc3c9fc0992
GudkovSergey <gudkow-sergey@mail.ru>
2023-02-06 16:34:20 +0500

    приступил к состоянию наведения

--- src/components/Packing/Packing.jsx
+++ src/components/Packing/Packing.jsx
@@ -1,34 +1,53 @@
 import React from "react";
 import style from "./Packing.module.scss"
 import cat from "../../Assets/cat.png"
+import cn from "classnames"
 
 const Packing = (props) => {
+    const [selected, setSelected] = React.useState(false)
+    const [selectedHover,setSelectedHover] =React.useState(false)
+
+    const selectPacking = ()=> {
+        setSelected(selected === false ? true : false);
+
+    }
+    const selectPackingHover = ()=>{
+        if(selected===true){
+
+        }
+    }
+
     return (
         <div className={style.packing_wrap}>
 
-            <div className={style.packing_container}>
+            <div onMouseLeave={selectPackingHover}
+
+                 onClick={selectPacking}
+
+                 className={selected === false ? cn(style.packing_container, style.defaultHover) : cn(style.packing_container, style.activeSelected)}>
 
                 <div className={style.packing_info__block}>
 
-                  <div className={style.packing_tagline}>{props.tagline}</div>
-                  <div className={style.packing_title}>{props.title}</div>
-                  <div className={style.packing_topping}>{props.topping} </div>
-                  <div className={style.packing_portion}>{props.portion} порций</div>
+                    <div className={style.packing_tagline}>{props.tagline}</div>
+                    <div className={style.packing_title}>{props.title}</div>
+                    <div className={style.packing_topping}>{props.topping} </div>
+                    <div className={style.packing_portion}>{props.portion} порций</div>
 
-                  <div className={style.packing_bonus__container}>
-                      <div className={style.packing_bonus__quantity}>{props.bonus_quantity} </div>
-                      <div className={style.packing_bonus}>{props.bonus}  </div>
+                    <div className={style.packing_bonus__container}>
+                        <div className={style.packing_bonus__quantity}>{props.bonus_quantity} </div>
+                        <div className={style.packing_bonus}>{props.bonus}  </div>
 
-                  </div>
-                      <div className={style.packing_comment}>{props.comment}  </div>
-                  </div>
+                    </div>
+                    <div className={style.packing_comment}>{props.comment}  </div>
+                </div>
 
 
                 <div className={style.packing_visualBlock}>
 
                     <img className={style.packing_cat} src={cat} alt="cat"/>
 
-                    <div className={style.packing_volume__box}>
+                    <div
+                        className={selected === false ? style.packing_volume__box : cn(style.packing_volume__box, style.volume_box__active)}>
 
                         <div className={style.packing_volume}>{props.volume} </div>
 
@@ -38,7 +57,14 @@ const Packing = (props) => {
                 </div>
             </div>
 
-            <div className={style.packing_description}>{props.descriotion_default} <a href=''>купи</a></div>
+            {
+                selected === false ?
+                    <div
+                        className={style.packing_description}>{props.description_default}
+                        <a onClick={() => setSelected(selected === false ? true : false)} href='#'>купи</a>
+                    </div> :
+                    <div className={style.packing_description}>{props.description_selected} </div>
+            }
         </div>
 
     )
--- src/components/Packing/PackingContainer.jsx
+++ src/components/Packing/PackingContainer.jsx
@@ -4,22 +4,24 @@ import Packing from "./Packing";
 import cart from "../../Assets/cart.json";
 
 const PackingContainer = ()=>{
+
     return(
         <div className={style.packing}>
 
             {
                 cart.map((obj) =>(
-                    <Packing
-                         key={obj.id}
-                         tagline={obj.tagline}
-                         title={obj.title}
-                         topping ={obj.topping}
-                         portion={obj.portion}
-                         bonus={obj.bonus}
-                         bonus_quantity={obj.bonus_quantity}
-                         comment={obj.comment}
-                         volume={obj.volume}
-                         descriotion_default={obj.descriotion_default}
+                    <Packing {...obj}
+                         // key={obj.id}
+                         // tagline={obj.tagline}
+                         // title={obj.title}
+                         // topping ={obj.topping}
+                         // portion={obj.portion}
+                         // bonus={obj.bonus}
+                         // bonus_quantity={obj.bonus_quantity}
+                         // comment={obj.comment}
+                         // volume={obj.volume}
+                         // description_default={obj.description_default}
+
 
                 />))
             }
